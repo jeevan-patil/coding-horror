ee6d97959a4e141ba6aed27dc8c7f6f58d08ae75
Jeevan Patil <jeevanpaatil@gmail.com>
2018-07-25 11:33:12 +0530

    heavy pill problem

--- src/xyz/jeevan/code/arraystrings/PalindromePermutation.java
+++ src/xyz/jeevan/code/arraystrings/PalindromePermutation.java
@@ -1,16 +1,38 @@
 package xyz.jeevan.code.arraystrings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a string, check if it is a permutation of a palindrome.
+ *
+ * For example - Any permutation of "code" can not be a palindrome. However, permutation of "aab"
+ * can be a palindrome.
  *
  * @author jeevan
  */
 public class PalindromePermutation {
 
   public static void main(String[] args) {
-    System.out.println(new PalindromePermutation().isPermutationOfPalindrome("abcba"));
+    System.out.println(new PalindromePermutation().isPermutationOfPalindrome("aab"));
   }
 
+  /**
+   * Implementation using Set of characters.
+   */
+  boolean canPermutePalindrome(String s) {
+    Set<Character> set = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (!set.add(s.charAt(i))) {
+        set.remove(s.charAt(i));
+      }
+    }
+    return set.size() <= 1;
+  }
+
+  /**
+   * Implementation using array.
+   */
   boolean isPermutationOfPalindrome(String word) {
     int oddCount = 0;
     int[] characters = new int[Character.getNumericValue('z') -
---
+++ src/xyz/jeevan/code/puzzles/HeavyPillProblem.java
@@ -0,0 +1,101 @@
+package xyz.jeevan.code.puzzles;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Problem statement - You have 20 bottles of pills. 19 bottles have 1.0 gram pills, but one has
+ * pills of weight 1.1 grams. Given a scale that provides an exact measurement, how would you find
+ * the heavy bottle? You can only use the scale once. In programming terms, using the scale only
+ * once means iterate the collection only once.
+ *
+ * @author jeevan
+ */
+public class HeavyPillProblem {
+
+  private static int NUMBER_OF_BOTTLES = 20;
+  private static double DEFAULT_PILL_WEIGHT = 1.0;
+  private static double FAULTY_PILL_WEIGHT = 1.1;
+
+  public static void main(String[] args) {
+    int heavyBottle = new HeavyPillProblem().findHeavyBottle(getBottleContainer());
+    System.out.println("Heavy bottle number is : " + heavyBottle);
+  }
+
+  // prepare bottle data
+  private static Container getBottleContainer() {
+    Random random = new Random();
+    int bottleWithHeavyPill = random.nextInt(NUMBER_OF_BOTTLES) + 1;
+    System.out.println(bottleWithHeavyPill);
+
+    List<Bottle> bottles = new LinkedList<>();
+    double weight = 0.0;
+    for (int i = 1; i <= NUMBER_OF_BOTTLES; i++) {
+      double bottleWeight = (i == bottleWithHeavyPill ? FAULTY_PILL_WEIGHT : DEFAULT_PILL_WEIGHT);
+      weight += bottleWeight;
+      bottles.add(new Bottle(i, bottleWeight));
+    }
+    Container container = new Container(bottles);
+    container.setWeight(weight);
+    return container;
+  }
+
+  public int findHeavyBottle(Container container) {
+    double totalWeight = container.getWeight();
+    List<Bottle> bottles = container.getBottles();
+
+    double idealWeight = 0;
+    for (int i = 1; i < bottles.size(); i++) {
+      idealWeight += (i * DEFAULT_PILL_WEIGHT);
+    }
+
+    System.out.println("total weight - " + totalWeight);
+    System.out.println("ideal weight - " + idealWeight);
+
+    Double heavyPill = (totalWeight - idealWeight) / DEFAULT_PILL_WEIGHT;
+
+    return heavyPill.intValue();
+  }
+}
+
+class Container {
+
+  private List<Bottle> bottles;
+  private double weight;  // total weight of bottles
+
+  public Container(List<Bottle> bottles) {
+    this.bottles = bottles;
+  }
+
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+
+  public List<Bottle> getBottles() {
+    return bottles;
+  }
+
+  public double getWeight() {
+    return weight;
+  }
+}
+
+class Bottle {
+
+  private int id;
+  private double weightOfPill;
+
+  public Bottle(int id, double weightOfPill) {
+    this.id = id;
+    this.weightOfPill = weightOfPill;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public double getWeightOfPill() {
+    return weightOfPill;
+  }
+}
