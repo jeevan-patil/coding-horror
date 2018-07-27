a37f67ff8b03547fb002c3d83971fe039fb8b1a5
Jeevan Patil <jeevanpaatil@gmail.com>
2018-07-27 11:44:13 +0530

    number to ordinal problem

---
+++ src/xyz/jeevan/code/problems/NumberToOrdinal.java
@@ -0,0 +1,29 @@
+package xyz.jeevan.code.problems;
+
+import java.util.logging.Logger;
+
+/**
+ * Convert 1 -> 1st, 2 -> 2nd, 31 -> 31st, 12 -> 12th
+ */
+public class NumberToOrdinal {
+
+  private static Logger log = Logger.getLogger("NumberToOrdinal");
+
+  public static void main(String[] args) {
+    log.info(new NumberToOrdinal().convert(113));
+  }
+
+  String convert(final int number) {
+    String[] ordinals = {"th", "st", "nd", "th", "th", "th", "th", "th", "th", "th"};
+    int modBy100 = number % 100;
+
+    switch (modBy100) {
+      case 11:
+      case 12:
+      case 13:
+        return number + "th";
+      default:
+        return number + ordinals[number % 10];
+    }
+  }
+}
