c4e4c9ca227cbd624353db15738968c5af5936f4
Jeevan Patil <jeevanpaatil@gmail.com>
2018-08-14 15:42:11 +0530

    refactored the code to find out the max profit in just single for loop

--- src/xyz/jeevan/code/profit/MaxProfit.java
+++ src/xyz/jeevan/code/profit/MaxProfit.java
@@ -1,24 +1,27 @@
 package xyz.jeevan.code.profit;
 
+/**
+ * Calculate the max profit one can earn. Given an array of prices of shares, the index define the
+ * day at which value was the price.
+ */
 public class MaxProfit {
 
   public static void main(String[] args) {
-    int[] data = {2, 5, 6, 1, 3, 0};
-
+    int[] data = {10, 5, 3, 1, 0, 4, 2, 3, 9};
     System.out.println(new MaxProfit().maxProfit(data));
   }
 
   public int maxProfit(int[] prices) {
-    int maxProfit = 0;
+    int maxPrice = 0;
+    int minPrice = prices[0];
 
-    for (int i = 0; i < prices.length; i++) {
-      for (int j = (i + 1); j < prices.length; j++) {
-        if ((prices[j] - prices[i]) > maxProfit) {
-          maxProfit = (prices[j] - prices[i]);
-        }
+    for (int i = 0; i <= prices.length - 2; i++) {
+      if (prices[i + 1] > prices[i]) {
+        maxPrice = prices[i + 1];
+        minPrice = (prices[i] < minPrice ? prices[i] : minPrice);
       }
     }
 
-    return maxProfit;
+    return (maxPrice - minPrice);
   }
 }
